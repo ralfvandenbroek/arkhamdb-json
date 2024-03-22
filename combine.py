ef7ba90da15069c721c65006ff2bfca3ac225568
Ralf van den Broek <2565537+ralfvandenbroek@users.noreply.github.com>
2024-03-22 19:47:21 +0100

    Update combine.py

--- combine.py
+++ combine.py
@@ -4,9 +4,14 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 data = {}
 
-for filename in ['cycles', 'encounters', 'factions']:
+for filename in ['cycles', 'encounters', 'factions', 'packs', 'subtypes', 'taboos, 'types']:
   with open(f"{dir_path}/../json/{filename}.json", "r") as f:
     data[filename] = json.load(f)
 
+prefix = f"{dir_path}/../json/packs/"
+for filename in glob(f"{prefix}*/*.json"):
+  with open(filename, "r") as f:
+    data[filename[len(prefix):-5]] = json.load(f)
+
 with open(f"{dir_path}/combined.json", "w") as f:
-  json.dump(data, f)
+  json.dump(data, f, indent=2)
